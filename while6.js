d5400bdd4eedfd78aafb8d7c4113276280cf4646
Euzebio <euzebiosantanawork@gmail.com>
2020-05-22 17:33:28 -0300

    version1.0

---
+++ while6.js
@@ -0,0 +1,11 @@
+/* faca um programa que pergunte ao usuario:
+Insira zero para sair, ou qualquer outra tecla para continuar"
+Se o usuario inserir zero ele sai, se nao o programa reinicia
+novamente*/
+var rs= require("readline-sync")
+
+while (answer!=0){
+ 
+ var answer =rs.question("Enter zero to exit or any other key to continue:")
+  
+}
