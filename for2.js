1804f89fc39788e0533f6aabba437f015cb90b66
Euzebio <euzebiosantanawork@gmail.com>
2020-05-22 17:08:21 -0300

    version1.0

---
+++ for2.js
@@ -0,0 +1,16 @@
+/*
+
+Faca um programa que leia dois numeros, o primeiro e o valor 
+inicial de um contador, e o segundo e o valor final do contatador
+(verifique se o valor incial e inferior ao valor final). 
+*/
+
+var rs = require("readline-sync")
+var start = rs.questionInt(" type a number to be your accountant:")
+var end= rs.questionInt(" type a number that finalizes your \
+accountant:")
+
+for( var i = start; i <= end; i++);{
+    console.log(start)
+   
+   }
