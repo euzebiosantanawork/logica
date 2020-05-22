57b3ed20178ff49bfde7fb2d8d4f602f4ea4baaf
Euzebio <euzebiosantanawork@gmail.com>
2020-05-22 17:08:57 -0300

    version1.0

---
+++ for3.js
@@ -0,0 +1,9 @@
+/* 
+Faca um programa que escreve na tela os numeros pares entre
+0 e 5o usando o comando for. nao utilize nenhum if them neste 
+programa apenas o comando For-Do.
+*/
+
+for(i=0; i<=50; i = i+2){
+    console.log(i)
+}
