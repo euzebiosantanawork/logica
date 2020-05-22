36333cef9a89390c653b366f98a358d4c0d15bce
Euzebio <euzebiosantanawork@gmail.com>
2020-05-22 17:14:00 -0300

    version1.0

---
+++ if_inter5.js
@@ -0,0 +1,3 @@
+/*faca um porgrama que calcule e imprima o valor em reais de cada kw, o valor em reais
+a ser pago o novo valor a ser pago por essa residencia com o desconto de 10%. dado 100 
+kilowatts custa 1/7 do salario minimo. quantidade de kw gasto por residencia.*/
