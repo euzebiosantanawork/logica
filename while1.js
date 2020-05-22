8e09ac6c075527f76543f71094e0bce6eaaa8b43
Euzebio <euzebiosantanawork@gmail.com>
2020-05-22 17:19:07 -0300

    version1.0

---
+++ while1.js
@@ -0,0 +1,15 @@
+/* Faca um programa que recebe um numero de entrada e repita a mensagem
+"BIP BIP" de acordo com o numero inserido*/
+
+var rs= require("readline-sync")
+var number = rs.question ("Type a number: ")
+
+var cont = 0
+while(cont < number){
+    console.log("BIP BIP")
+    cont ++
+} if (num ==0 ) {
+ console.log('You wrote the number zero, try again')
+} else if (number > 0) {
+    console.log ("You wrote the number zero, try again")
+}
