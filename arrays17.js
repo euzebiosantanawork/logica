8ce3c50d6f77302361b3fba886afaa420542d37b
Euzebio <euzebiosantanawork@gmail.com>
2020-06-11 09:09:26 -0300

    version1.0

---
+++ arrays17.js
@@ -0,0 +1,5 @@
+// 17. Faça um algoritmo que receba um texto e imprima ele em letra maisucula novamente.
+// Utilize o método toUpperCase()
+var rs = require("readline-sync");
+var text = rs.question("Type a text: ");
+console.log(text.toUpperCase())
