656972708bd4659cdd2787672c45221c2cf4ac14
Euzebio <euzebiosantanawork@gmail.com>
2020-06-11 08:37:04 -0300

    version1.0

--- arrays1.js
+++ arrays1.js
@@ -1,23 +1,50 @@
-var rs = require ("readline-sync")
-var product = []
-var total = 0
+/*
+1. Faça um algoritmo em que o usuário possa cadastrar preços de produtos e no final
+exiba o preço de cada produto junto com o valor total. após inserir todos os produtos o
+programa deve pedir o valor a ser pago, caso o valor passe do da compra exibir o troco,
+se o valor for menor informar que a compra vai ser cancelada.
+EX entradas :
+0.75
+0.25
+2.50
+Valor total pago: 5.00
+O programa imprime :
+Produto 1 - 0.75
+Produto 2 - 0.25
+Produtos 3 - 2.50
+Total da compra : 3.50 $
+Troco : 1.50 $
+*/
 
-while (true){
-    var answer = rs.question("Type K to continue, and E to exit: ")
-    if(answer == "K"){
-        var price = rs.question ("Enter the product value: ")
-        total = total + price
-        product.push (price)
-    }else{
-        console.log ("Thank you!");
-        break;
-    }
+var rs = require ("readline-sync")
+var numberofProducts = rs.questionInt ("How many products\
+ do you want to buy?")
+var prices = [] ;
+for (var counter= 0 ; counter < numberofProducts ; counter ++){
+    var price = rs.questionFloat("Type the price of the product: ");
+    prices . push (price);
 }
 
-for (var i = 0; i < product.length; i++) {
-    var cont = i + 1
-    console.log (`Product ${cont} = ${product[i]}`)
-} 
-console.log(`total purchase ${total}`reais)
-var payment = rs.questionFloat (`Total de compra ${total} reias`)
+
+
+var payment = rs.questionFloat (" Pay ( We do not accept coins): ")
+var totalPurchase = 0;
+
+for (var counter = 0; counter < prices.length; counter ++) {
+    var currentProductPrice = prices [counter];
+    console.log (`Product ${counter}: R$ ${currentProductPrice}`);
+    var totalPrice = totalPurchase + currentProductPrice ;
+    totalPurchase = totalPrice 
+}
+
+console.log(`Total purchase: R${totalPurchase}`);
+ if (totalPurchase > payment){
+      console.log ('Don`t have enugh money')
+ } else if (payment > totalPurchase){
+     console.log (`I'll get the chage...`);
+     var change = payment - totalPurchase;
+    console.log(`Your change ${change}, thank you! `)
+ } else {
+     console.log ('You gave the exact money, thanks!')
+ }
 
