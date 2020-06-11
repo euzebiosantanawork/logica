0dd79d2ebe322a3b206c3bb64230987bf5b32265
Euzebio <euzebiosantanawork@gmail.com>
2020-06-11 09:10:55 -0300

    version1.0

---
+++ arrays19.1.js
@@ -0,0 +1,10 @@
+  
+// 19. Faça um algoritmo que receba um texto e remova todos os espaços em brancos no
+// começo e no final do texto
+// Utilize o método trim()
+
+
+var rs = require("readline-sync");
+var texto = rs.question("Informe o textpo");
+
+console.log(texto.trim());
