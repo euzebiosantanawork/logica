466f5f5565480b40f9143b731b9fc9d6b34e0fcf
Euzebio <euzebiosantanawork@gmail.com>
2020-06-11 09:10:06 -0300

    version1.0

---
+++ arrays18.js
@@ -0,0 +1,7 @@
+
+/*18. Faça um algoritmo que receba um texto e imprima ele em letra minuscula novamente.
+Utilize o método toLowerCase()*/
+
+var rs = require("readline-sync");
+var text = rs.question("Type a text: ")
+console.log(text.toLowerCase())
