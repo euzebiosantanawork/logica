0d570ce2b14f8adcb8ed878798d672c920c6ae8b
Euzebio <euzebiosantanawork@gmail.com>
2020-06-25 17:15:57 -0300

    version1.0

---
+++ functions4.js
@@ -0,0 +1,13 @@
+var rs = require("readline-sync");
+
+function multiplicationTable(n){
+    var a = [];
+    for(var i = 1; i <= n; i++){
+        var j = n*i
+        a.push(j)
+    }
+    z = console.log(a)
+    return z;
+}
+
+console.log(mu
