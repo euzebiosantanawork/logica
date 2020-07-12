56e967a3d622c86a28a267a41722e31707a0f7be
Euzebio <euzebiosantanawork@gmail.com>
2020-07-12 19:59:34 -0300

    version1.0

---
+++ class02.00.js
@@ -0,0 +1,29 @@
+let rs =require("readline-sync");
+
+class Students{
+    constructor (name,age){
+        this.name = name;
+        this.age = age;
+
+    };
+
+};
+
+let students = [];
+
+for (let cont = 0; cont <= 2 ; cont++){
+
+}
+
+console.log( "Thank you for using our program (:\
+    \
+            This program is being developed by\
+          Euzebio Santana,\
+          \
+          Contacts:\
+          WHATSSAP +5511951732589\
+          LINKEDIN: https: //www.linkedin.com/in/robertfredericksmith/\
+          GITHUB: https: //github.com/euzebiosantanawork\
+          SITE: https: //sites.google.com/view/euzebiosantana/home\
+          \
+            -Check back often!" );
