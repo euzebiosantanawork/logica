5c877fdeaa304d5b7ace3f33b276ae0e97a6d40a
Euzebio <euzebiosantanawork@gmail.com>
2020-07-12 19:55:34 -0300

    version1.0

---
+++ modules04.00.js
@@ -0,0 +1,23 @@
+var rs =require('readline-sync');
+var isPrime = require('prime-number');
+var n = isPrime(rs.questionInt("Type a number: "));
+
+if(n == true){
+    console.log('Is prime! ');
+}else{
+    console.log("Not is prime !");
+    
+}
+
+console.log( "Thank you for using our program (:\
+    \
+            This program is being developed by\
+          Euzebio Santana,\
+          \
+          Contacts:\
+          WHATSSAP +5511951732589\
+          LINKEDIN: https: //www.linkedin.com/in/robertfredericksmith/\
+          GITHUB: https: //github.com/euzebiosantanawork\
+          SITE: https: //sites.google.com/view/euzebiosantana/home\
+          \
+            -Check back often!" );
