8a8aa1e2cc71644ecaa17abfe772c5a63ece6754
Euzebio <euzebiosantanawork@gmail.com>
2020-07-12 19:54:49 -0300

    version1.0

---
+++ modules03.00.js
@@ -0,0 +1,22 @@
+var rs = require("reeadline-sync");
+var odd = require('is-odd');
+var n = impar (rs.questionInt("Type a number: "));
+
+if(n==true){
+    console.log("is odd")
+}else{
+     console.log("is even")
+}
+
+console.log( "Thank you for using our program (:\
+    \
+            This program is being developed by\
+          Euzebio Santana,\
+          \
+          Contacts:\
+          WHATSSAP +5511951732589\
+          LINKEDIN: https: //www.linkedin.com/in/robertfredericksmith/\
+          GITHUB: https: //github.com/euzebiosantanawork\
+          SITE: https: //sites.google.com/view/euzebiosantana/home\
+          \
+            -Check back often!" );
